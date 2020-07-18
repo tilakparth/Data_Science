e3d0db3ea017ca41c0f1403e46d58b9023d694a3
tilakparth <tilak@iitk.ac.in>
2020-07-18 17:59:45 +0530

    data visulization and web scrapping

---
+++ OOPs/main.py
@@ -0,0 +1,17 @@
+import calculator
+
+
+print("hello world !")
+
+
+def func():
+    print("Hello")
+
+func()
+
+print(calculator.add(1, 3))
+print(calculator.mul(1, 3))
+print(calculator.sub(1, 3))
+print(calculator.div(1, 3))
+
+print(calculator.mul(5, 21125))
